d96267da476557f947e088fb0e087bfdabdfabab
unicornwizard375 <drumm040@morris.umn.edu>
2019-11-12 08:39:25 -0600

    update initial attempt at the multithreaded client.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -3,6 +3,7 @@ package echoserver;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.ConnectException;
 import java.net.Socket;
 
 public class EchoClient {
@@ -18,6 +19,31 @@ public class EchoClient {
 		InputStream socketInputStream = socket.getInputStream();
 		OutputStream socketOutputStream = socket.getOutputStream();
 
-		// Put your code here.
+		Thread keyboardRead = new Thread(() ->{
+		try {
+			int line;
+			while ((line = System.in.read()) != -1) {
+				socketOutputStream.write(line);
+				int newLine = socketInputStream.read();
+				//System.out.write(newLine);
+			}
+			socket.shutdownOutput();
+			System.out.flush();
+			socket.close();
+		}
+		catch (ConnectException ce) {
+			System.out.println("We were unable to connect to the host");
+			System.out.println("You should make sure the server is running.");
+		} catch (IOException ioe) {
+			System.out.println("We caught an unexpected exception");
+			System.err.println(ioe);
+		}
+	});
+
+		keyboardRead.start();
+
+
+
 	}
+
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -3,6 +3,7 @@ package echoserver;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.ConnectException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,13 +16,44 @@ public class EchoServer {
 	}
 
 	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
-			InputStream inputStream = socket.getInputStream();
-			OutputStream outputStream = socket.getOutputStream();
+		try {
+			ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+			while (true) {
+				Socket socket = serverSocket.accept();
+				System.out.println("Request accepted");
+				InputStream inputStream = socket.getInputStream();
+				OutputStream outputStream = socket.getOutputStream();
 
-			// Put your code here.
+					Thread readInput = new Thread(() -> {
+						try {
+							int line;
+							while ((line = inputStream.read()) != -1) {
+								outputStream.write(line);
+								System.out.println("Passed through here: " + line);
+							}
+
+
+							socket.shutdownOutput();
+							System.out.flush();
+							socket.close();
+
+						} catch (ConnectException ce) {
+							System.out.println("We were unable to connect to the host");
+							System.out.println("You should make sure the server is running.");
+						} catch (IOException ioe) {
+							System.out.println("We caught an unexpected exception");
+							System.err.println(ioe);
+						}
+					});
+
+
+					readInput.start();
+			}
+		}
+        catch (IOException ioe) {
+			System.out.println("We caught an unexpected exception");
+			System.err.println(ioe);
+		}
+
 		}
 	}
-}
